1b17d2ff6d1d33dd56176c5f9e27e81c30881f4d
Yourname <YourEmail>
2020-02-29 15:59:48 -0500

    update LocationMap.java

--- src/test/LocatingMap.java
+++ src/test/LocatingMap.java
@@ -3,5 +3,7 @@ package test;
 public class LocatingMap {
     public static void main(String[] args) {
         System.out.println("Locating map");
+
+        System.out.println("Changes from IntelliJ");
     }
 }
