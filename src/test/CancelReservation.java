bc52dec84016a696b51a818fdc1bf74242be4059
edilbekbrowne <58631727+edilbekbrowne@users.noreply.github.com>
2020-02-29 15:54:43 -0500

    Update CancelReservation.java

--- src/test/CancelReservation.java
+++ src/test/CancelReservation.java
@@ -4,7 +4,7 @@ public class CancelReservation {
     public static void main(String[] args) {
         System.out.println("you have canceed your reservation");
         
-         System.out.println("Goodbye Git"); // this line us frim github
+         System.out.println("Hello GitHub"); // this line us frim github
         
     }
 }
