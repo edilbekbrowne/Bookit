c074630dcdd1094c3d59f25837415a5fc915d409
edilbekbrowne <58631727+edilbekbrowne@users.noreply.github.com>
2020-02-29 14:57:41 -0500

    Update Login.java

--- src/test/Login.java
+++ src/test/Login.java
@@ -3,5 +3,7 @@ package test;
 public class Login {
     public static void main(String[] args) {
         System.out.println("User logged in to Bookit");
+        System.out.println("User logged in to Bookit changed through GITHUB");
+        System.out.println("User logged in to Bookit");
     }
 }
